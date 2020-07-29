8a7a4ca8242b5e83a2435675fd8cb24b1be6bb2c
Jacob <jacob1107@163.com>
2020-07-29 11:09:06 +0800

    commit  fist

---
+++ src/main/java/com/spring/jacob/SpringJacobApplication.java
@@ -0,0 +1,27 @@
+package com.spring.jacob;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringJacobApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(SpringJacobApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJacobApplication.class, args);
+	}
+
+	@RequestMapping("/say")
+	public String say() {
+
+		log.info("========================={}", Math.random());
+		return String.valueOf(Math.random());
+
+	}
+}
